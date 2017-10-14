23a62d91efb0164587a3ef197e33dd0ebd8ab8b3
littlemm <785897146@qq.com>
2017-10-14 22:33:28 +0800

    update

--- Spider/Spider/Request.cs
+++ Spider/Spider/Request.cs
@@ -14,10 +14,21 @@ namespace Spider
 
         public Request(string uri)
         {
+            RequestUri = uri;
            // webRequest = new HttpWebRequest();
+        }
+
+        public async void GenerateWebRequest()
+        {
+            webRequest = (HttpWebRequest)WebRequest.Create(RequestUri);
+            webRequest.Method = "GET";
+            webResponse = (HttpWebResponse)await webRequest.GetResponseAsync();
+            ContentStream = webResponse.GetResponseStream();
+
         }
         string RequestUri { get; set; }
 
+        private HttpWebResponse webResponse;
         private HttpWebRequest webRequest;
         private UInt32 Index;   //编号
         private Stream ContentStream;
--- Spider/Spider/WorkManage.cs
+++ Spider/Spider/WorkManage.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spider;
 
 namespace Spider
 {
     class WorkManage
     {
-        int RequestCount { get; set; }
+        int RequestCount { get; set; } //并行工作数量
         bool [] IsFinished { get; set; }
-
+        bool [] WorkBusy { get; set; }
         List<string> finisheduri;
         List<string> unfinisheduri;
 
         void RunTask()
         {
+            for(int i=0;i<RequestCount;i++)
+            {
+                GenerateWork(i);
+            }
+
+        }
+        void GenerateWork(int i)
+        {
+            WorkBusy[i] = true;
+
             string uri = unfinisheduri.First();
             unfinisheduri.Remove(uri);
             finisheduri.Add(uri);
+
+            Request request = new Request(uri);
 
         }
     }
